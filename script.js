1afb58aeab831ce934567fb74ee8097a80f482e7
somber-robot <somberrobot@gmail.com>
2025-06-25 23:08:21 +0100

    Add keyboard support

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const divide = (a,b) => {
 };
 
 let result;
+let stack = [];
 function operate(){
     switch(stack[1]){
         case "+": result = add(stack[0],stack[2]); break;
@@ -25,112 +26,120 @@ function operate(){
     newNum = true;
 }
 
-let stack = [];
-
-// SCREEN
 const screen = document.querySelector("#display-content");
 let display = "";
 const setDisplay = () => screen.innerHTML = display;
 
-// DIGITS
 const digitBtns = document.querySelectorAll(".digit");
 const MAX = 12;
 let newNum = true;
 
+function addDigit(digit){
+    if (display.length >= MAX) return;
+    if (digit === "0" && display === "0") return;
+    
+    if (display === "0" || newNum) {
+        display = digit;
+        newNum = false;
+    }
+    else display += digit;
+    
+    setDisplay();
+}
+
 digitBtns.forEach(button =>{
     button.addEventListener("click", function(){
-        if (display.length >= MAX) return;
-        if (button.innerHTML === "0" && display === "0") return;
-        
-        if (display === "0" || newNum) {
-            display = button.innerHTML;
-            newNum = false;
-        }
-        else display += button.innerHTML;
-        
-        setDisplay();
+        addDigit(button.innerText);
     });
 });
 
-// DELETE
 const delBtn = document.querySelector(".delete");
 
-delBtn.addEventListener("click", function(){
+function del(){
     if (display.length == 0) return;
     display = display.substring(0, display.length-1);
     setDisplay();
     newNum = false;
-});
+}
 
+delBtn.addEventListener("click", del);
 
-// CLEAR
 const clearBtn = document.querySelector(".clear");
 
-clearBtn.addEventListener("click", function(){
+function clear(){
     display = "";
     setDisplay();
     stack = [];
-    console.log("stack: " + stack);
-});
+}
 
-// SIGNS
+clearBtn.addEventListener("click", clear);
+
 const signBts = document.querySelectorAll(".sign");
+
+function addSign(sign){
+    switch(stack.length){
+        case 0:
+            if (display === "") return;
+            stack.push(+display);
+            stack.push(sign);
+            display = "";
+            setDisplay();
+            break;
+        case 1:
+            if (display === "") stack = [];
+            else {
+                newNum = true;
+                stack.pop();
+                stack.push(+display);
+                stack.push(sign)
+            }
+            break;
+        case 2:
+            if (display === "" || newNum){
+                stack.pop();
+                stack.push(sign);
+            }else{
+                stack.push(+display);
+                operate();
+                stack.push(sign);
+            }
+            break;
+    }
+}
 
 signBts.forEach(button => {
     button.addEventListener("click", function(){
-        switch(stack.length){
-            case 0:
-                if (display === "") return;
-                stack.push(+display);
-                stack.push(button.innerText);
-                display = "";
-                setDisplay();
-                break;
-            case 1:
-                if (display === "") stack = [];
-                else {
-                    newNum = true;
-                    stack.pop();
-                    stack.push(+display);
-                    stack.push(button.innerText)
-                };
-                break;
-            case 2:
-                if (display === "" || newNum){
-                    stack.pop();
-                    stack.push(button.innerText);
-                }else{
-                    stack.push(+display);
-                    operate();
-                    stack.push(button.innerText);
-                }
-                break;
-        }
-        console.log("stack: " + stack);
+        addSign(button.innerText);
     });
 });
 
-// EQUALS TO
 const equalBtn = document.querySelector(".equal");
 
-equalBtn.addEventListener("click", function(){
-    switch(stack.length){
-        case 2:
-            if (display === "" || newNum) return;
-            stack.push(+display);
-            operate();
-            break;
-    }
-    console.log("stack: " + stack);
-});
+function equals(){
+    if (stack.length !== 2 || display === "" || newNum) return;
+    stack.push(+display);
+    operate();
+}
 
-// DOT
+equalBtn.addEventListener("click", equals);
+
 const dotBtn = document.querySelector(".dot");
 
-dotBtn.addEventListener("click", function (){
+function dot(){
     if (display.includes(".")) return;
     if (display === "" || newNum) display = "0.";
     else display += ".";
     setDisplay();
     newNum = false;
+}
+
+dotBtn.addEventListener("click", dot);
+
+document.body.addEventListener("keydown", function(event){
+    if ("0123456789".includes(event.key)) addDigit(event.key);
+    if (event.key === "Backspace") del();
+    if (event.key === "c") clear();
+    if ("+-*/".includes(event.key)) addSign(event.key);
+    if (event.key === "=" || event.key === "Enter") equals();
+    if (event.key === ".") dot();
 });
