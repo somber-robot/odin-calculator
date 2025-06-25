89b1c6668ac482f1248177af25fa67a2dc5a0b1b
somber-robot <somberrobot@gmail.com>
2025-06-25 21:16:31 +0100

    Implement equals to button

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const add = (a,b) => a+b;
-const subtract = (a,b) => a-b;
-const multiply = (a,b) => a*b;
+const add = (a,b) => Math.round((a+b) * 1000) / 1000;
+const subtract = (a,b) => Math.round((a-b) * 1000) / 1000;
+const multiply = (a,b) => Math.round((a*b) * 1000) / 1000;
 const divide = (a,b) => {
     if (b === 0) {
         alert("ERROR: Cannot divide by 0");
         return 0;   
     }
-    return a/b;
+    return Math.round((a/b) * 1000) / 1000;
 };
 
 let result;
@@ -79,22 +79,44 @@ signBts.forEach(button => {
         switch(stack.length){
             case 0:
                 if (display === "") return;
-                stack.push(parseInt(display));
+                stack.push(+display);
                 stack.push(button.innerText);
                 display = "";
                 setDisplay();
+                break;
+            case 1:
+                if (display === "") stack = [];
+                else {
+                    newNum = true;
+                    stack.pop();
+                    stack.push(+display);
+                    stack.push(button.innerText)
+                };
                 break;
             case 2:
                 if (display === "" || newNum){
                     stack.pop();
                     stack.push(button.innerText);
                 }else{
-                    stack.push(parseInt(display));
+                    stack.push(+display);
                     operate();
                     stack.push(button.innerText);
                 }
                 break;
         }
-        console.log("stack: " + stack);
     });
+});
+
+// EQUALS TO
+const equalBtn = document.querySelector(".equal");
+
+equalBtn.addEventListener("click", function(){
+    switch(stack.length){
+        case 2:
+            if (display === "") return;
+            stack.push(+display);
+            operate();
+            newNum = false;
+            break;
+    }
 });
