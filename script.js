77c71000d56b94d02463147a91a49dbc05a028a8
somber-robot <somberrobot@gmail.com>
2025-06-25 13:36:10 +0100

    Implement basic input

--- script.js
+++ script.js
@@ -9,85 +9,39 @@ const divide = (a,b) => {
     return a/b;
 };
 
-let num1, sign, num2;
-let state = 0;
-let new_num = true;
-
-function operate(s, a, b){
-    num1 = num2 = sign = null;
-    switch(s){
-        case '+': result = add(a, b); break;
-        case '-': result = subtract(a, b); break;
-        case '*': result = multiply(a, b); break;
-        case '/': result = divide(a, b); break;
+function operate(sign, a, b){
+    switch(sign){
+        case "+": return add(a,b);
+        case "-": return subtract(a,b);
+        case "/": return divide(a,b);
+        case "*": return multiply(a,b);
     }
-    display = `${result}`;
-    setDisplay();
-    new_num = true;
-    return result;
 }
 
+let stack = [];
 
-const digitBtns = document.querySelectorAll(".digit");
-const signBtns = document.querySelectorAll(".sign");
-const equalBtn = document.querySelector(".equal");
+// SCREEN
 const screen = document.querySelector("#display-content");
-
 let display = "";
-const MAX=12;
-const setDisplay = () => screen.innerText = display;
+const setDisplay = () => screen.innerHTML = display;
 
-digitBtns.forEach(button => {
+// DIGITS
+const digitBtns = document.querySelectorAll(".digit");
+const MAX = 12;
+
+digitBtns.forEach(button =>{
     button.addEventListener("click", function(){
-        if (display.length >= MAX && !new_num) return;
-        if (!new_num) display += button.innerText;
-        else {
-            display = button.innerText;
-            new_num = false;
-        }
+        if (display.length >= MAX) return;
+        display += button.innerHTML;
         setDisplay();
     });
 });
 
-let result;
+// DELETE
+const delBtn = document.querySelector(".delete");
 
-signBtns.forEach(button => {
-    button.addEventListener("click", function(){
-        console.log(`num1: ${num1} num2: ${num2} sign: ${sign}`);
-        if (display === "" && state === 0) return;
-
-        if (display === "" || new_num){
-            sign = button.innerText;
-            console.log(`num1: ${num1} num2: ${num2} sign: ${sign}`);
-            return;
-        }
-
-        switch(state){
-            case 0:
-                num1 = parseInt(display);
-                display = "";
-                setDisplay();
-                sign = button.innerText;
-                state = 1;
-                break; 
-            case 1:
-                num2 = parseInt(display);
-                result = operate(sign, num1, num2);
-                num1 = result;
-                sign = button.innerText;
-                break;
-        }
-        console.log(`num1: ${num1} num2: ${num2} sign: ${sign}`);
-    });
-});
-
-equalBtn.addEventListener("click", function(){
-    console.log(state);
-    if (display === "" && state === 0) return;
-
-    if (state === 1 && !new_num){
-        num2 = parseInt(display);
-        result = operate(sign, num1, num2);
-        num1 = result;
-    }
+delBtn.addEventListener("click", function(){
+    if (display.length == 0) return;
+    display = display.substring(0, display.length-1);
+    setDisplay();
 });
